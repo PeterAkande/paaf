356fc563e6d69313e753eb85853f20e3f72ab854
Akande peter <akandepeter86@gmail.com>
2026-02-09 12:37:07 +0100

    feat: ensure logger handler is added only if not present

--- paaf/config/logging.py
+++ paaf/config/logging.py
@@ -4,13 +4,15 @@ import logging
 def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
 
-    logger.setLevel(logging.DEBUG)
+    # Only add handler if logger doesn't already have one
+    if not logger.handlers:
+        logger.setLevel(logging.DEBUG)
 
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
-    console_logger = logging.StreamHandler()
-    console_logger.setFormatter(formatter)
-    logger.addHandler(console_logger)
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        console_logger = logging.StreamHandler()
+        console_logger.setFormatter(formatter)
+        logger.addHandler(console_logger)
 
     return logger
